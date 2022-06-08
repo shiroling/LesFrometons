1a645273bda7db39fcd404efb2be864d18cd0ebf
shiroling <cintact.shiroling@gmail.com>
2022-06-09 01:47:53 +0200

    uyfg

--- src/mod├иleAnous/Main.java
+++
@@ -1,7 +0,0 @@
-package modèleAnous;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/mod├иleAnous/TypeVente.java
+++
@@ -1,5 +0,0 @@
-package modèleAnous;
-
-public enum TypeVente {
-    A_LA_COUPE_AU_POIDS, A_L_UNITE, ENTIER_OU_MOITIE, A_L_UNITE_PlUSIEURS_CHOIX, POUR_X_PERSONNES
-}
